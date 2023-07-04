84cc9227acff022842fdece2a698e1591d2aad58
Motufero <saulogcf@gmail.com>
2023-07-04 20:17:00 -0300

    função para checar tipos

--- lib/record.h
+++ lib/record.h
@@ -1,6 +1,8 @@
 #ifndef RECORD
 #define RECORD
 
+extern int yylineno;
+
 struct record {
   char * code; /* field for storing the output code */
   char * opt1; /* field for another purpose */
@@ -10,5 +12,8 @@ typedef struct record record;
 
 void freeRecord(record *);
 record * createRecord(char *, char *);
+
+
+void checkType(char* typeOne, char* typeTwo);
 
 #endif
